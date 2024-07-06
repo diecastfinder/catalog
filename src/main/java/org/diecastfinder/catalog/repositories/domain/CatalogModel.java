acc3f7770d2d68339fc561adaf9728c9d77698df
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-07-06 22:57:08 +0200

    issue2 - add H2 in-memory DB, pre-populate it on app start and make sure h2-console can be opened in browser.

---
+++ src/main/java/org/diecastfinder/catalog/repositories/domain/CatalogModel.java
@@ -0,0 +1,48 @@
+package org.diecastfinder.catalog.repositories.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.sql.Timestamp;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+@Entity
+public class CatalogModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String uri;
+    private String nameRequested;
+    private String lotTitle;
+    private String producer;
+    private Integer price;
+    private String currency;
+    private Timestamp firstFoundDate;
+    private Boolean isFavourite;
+    private Boolean isActive;
+}
