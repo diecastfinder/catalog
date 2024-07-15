9a0cf88a4378e7d095c46ad952f602d16e352c16
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-07-15 19:59:00 +0200

    issue18: add autodiscovery support

--- src/main/java/org/diecastfinder/catalog/CatalogApplication.java
+++ src/main/java/org/diecastfinder/catalog/CatalogApplication.java
@@ -2,7 +2,9 @@ package org.diecastfinder.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CatalogApplication {
 
